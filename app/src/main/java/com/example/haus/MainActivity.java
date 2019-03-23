a36c17e662ec448757d947c0bb990c2c7cda7e45
polina <p-leonova@list.ru>
2019-03-23 13:08:46 +0300

    для  haus

--- app/src/main/java/com/example/haus/MainActivity.java
+++ app/src/main/java/com/example/haus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.haus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +9,53 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    TextView textView;
+    Button but1;
+    Button but2;
+    Button but3;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void OnCreate(BundleSavedInstance){
+        super.OnCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        TextView = (TextView) findViewById(R.id.textView);
+        But1 = (Button) findViewById(R.id.but1);
+        But2 = (Button) findViewById(R.id.but2);
+        But3 = (Button) findViewById(R.id.but2);
+
+
+        But1.setOnClickListener (new View.OnClickListener(){
+
+                @Override
+                public void OnClick(View view){
+                    textView.setText("Нажата кнопка 1");
+                }
+
         });
+
+        But2.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void clickBut3(View view){
+       textView.setText(" Нажата кнопка 3");
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+@Override
+    public void OnClick(View view){
+        textView.setText("Нажата кнопка 2");
 }
+
+}
+
